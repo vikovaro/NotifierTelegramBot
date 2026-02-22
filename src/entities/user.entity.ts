5c5aa2861ce31ec8b62b722b6afc51e15f4d472b
admin <stychinskiisv@gmail.com>
2026-02-23 00:34:57 +0300

    WIP: remove sequelize model

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,22 +1,4 @@
-import { Table, Column, Model, DataType } from 'sequelize-typescript';
-
-@Table({ tableName: 'users' })
-export class User extends Model implements IUser {
-    @Column({
-        type: DataType.BIGINT,
-        primaryKey: true,
-        allowNull: false,
-    })
-    chatId: number;
-
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-    })
-    username: string;
-}
-
 export interface IUser {
-    chatId: number;
+    chatId: bigint;
     username: string;
 }
