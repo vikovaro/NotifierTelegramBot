8b2de01dea9ecc56bdc1a5c83b2d56937f2d370f
admin <stychinskiisv@gmail.com>
2026-02-24 16:24:51 +0300

    WIP: fix global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,30 +2,18 @@ import { Module } from '@nestjs/common';
 import { TelegramModule } from './telegram/telegram.module';
 import { UserModule } from './users/user.module';
 import { CoreModule } from './core/core.module';
-import { User } from './entities/user.entity';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ChatModule } from './chats/chat.module';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Module({
     imports: [
-        SequelizeModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                dialect: 'postgres',
-                host: configService.get<string>('DB_HOST'),
-                port: configService.get<number>('DB_PORT'),
-                username: configService.get<string>('DB_USERNAME'),
-                password: configService.get<string>('DB_PASSWORD'),
-                database: configService.get<string>('DB_NAME'),
-                models: [User],
-                autoLoadModels: true,
-                synchronize: true,
-            }),
-        }),
         CoreModule,
         TelegramModule,
+        ChatModule,
         UserModule,
     ],
+    providers: [
+        JwtAuthGuard,
+    ]
 })
 export class AppModule {}
